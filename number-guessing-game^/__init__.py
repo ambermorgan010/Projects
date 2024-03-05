d78f652cad91baaaf21de6e95c7510ada59643ce
Amber Morgan <ambermorgan010@gmail.com>
2024-03-05 02:07:51 -0500

    Pruning

--- number-guessing-game^/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Ignore this file. It is required to make Python see the files in this project
-as "importable".
-"""
--- qualitative-analysis^/amcq_solution.py
+++
@@ -1,6 +0,0 @@
-'''
-CSCI 120 - Computer Science I
-Instructors: Jeff Bush and Greg Schaper
-Description: Lab 8 - Making it Perfectly Readable
-Teammates Names:
-'''
